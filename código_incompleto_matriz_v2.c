f65f65f60bddce47d53d713934407aa5ecfa14b3
unknown <juniomiguelsizeg@gmail.com>
2021-03-06 15:55:12 -0300

    segunda versão incompleta mas com sintaxe interessante.

---
+++ código_incompleto_matriz_v2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{   
+    int a = 5;
+    int b = 5;
+    int i,j;
+    int matrizC[a][b];
+    
+    
+    while(b>0){
+        
+        printf("Digite o valor da linha da matriz: ");
+        
+        scanf("%d", &matrizC[0][b]);
+        
+        b--;
+        
+        if(b<=0){
+           
+            while(a>0){
+               
+               printf("Digite o valor da coluna da matriz: ");
+               
+               scanf("%d", &matrizC[a][0]); 
+               
+               a--;
+                
+            }
+        }
+    }
+    
+    for(i=0;i<b;i++){
+        printf("\t%d",matrizC[0][i]);
+        for(j=0;j<a;j++){
+            printf("\t%d\n",matrizC[j][0]);
+
+        }
+        
+    }
+    return 0;
+}
