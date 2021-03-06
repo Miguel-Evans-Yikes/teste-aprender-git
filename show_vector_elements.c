158968228ab050e5ef99a3262f04746ba75efac2
unknown <juniomiguelsizeg@gmail.com>
2021-03-06 12:01:58 -0300

    novo commit

---
+++ show_vector_elements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int SquareMatrix[5][5];
+    
+    printf("Digite os membros da matriz: ");
+    scanf("%d""%d""%d""%d""%d",&SquareMatrix[0][0],&SquareMatrix[0][1],&SquareMatrix[0][2],
+           &SquareMatrix[0][3],&SquareMatrix[0][4]);
+
+    printf("%d""%d""%d""%d""%d",SquareMatrix[0][0],SquareMatrix[0][1],SquareMatrix[0][2],
+           SquareMatrix[0][3],SquareMatrix[0][4]);
+    return 0;
+}
+
